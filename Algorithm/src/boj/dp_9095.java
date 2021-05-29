c7594867a5fe19b87c9974424e7937c2c9984d62
blues <blues@DESKTOP-S88GBVQ>
2021-05-29 20:56:47 +0900

    20210529_9095_123더하기

---
+++ Algorithm/src/boj/dp_9095.java
@@ -0,0 +1,34 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class dp_9095 {
+   public static void main(String[] args) throws IOException {
+      Scanner scanner = new Scanner(System.in);
+        
+        int t = scanner.nextInt();
+        int n;
+        
+        int[] output = new int[11]; //동적으로 생성시키니 메모리 초과가 되서 정적으로 선언
+        output[1] = 1; // 1을 넣을 경우 경우의 수 1가지(1)
+        output[2] = 2; // 2를 넣을 경우 경우의 수 2가지(1+1, 2)
+        output[3] = 4; // 3을 넣을 경우 경우의 수 3가지 (1+1+1, 1+2, 2+1, 3)
+        
+        for(int i=0; i<t; i++){
+            n = scanner.nextInt();
+            //4부터 점화식 적용,
+            //output4 = 1 + output(3)
+            //output4 = 2 + output(2)
+            //output4 = 3 + output(1)
+            //ㄸ라서 output4 = output(3) + output(2) + output(1)
+            for(int j=4; j<=n; j++){
+                output[j] = output[j-1] + output[j-2] + output[j-3];
+            }
+            System.out.println(output[n]);
+        }
+   }
+
+}
